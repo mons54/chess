504c022777092e6bc0316c904d11eaf6be35646c
Julien Simonet <j.mons54@gmail.com>
2017-01-30 04:38:52 +0100

    fix match game

--- server/modules/game.js
+++ server/modules/game.js
@@ -60,7 +60,7 @@ Game.prototype.create = function (socket, data) {
             (!match || value.createAt < game.createAt) &&
             (!value.pointsMin || value.pointsMin <= points) &&
             (!value.pointsMax || value.pointsMax >= points) &&
-            (!pointsMin || pointsMin <= value.points) &&
+            (!pointsMin || pointsMin <= value.pointsMin) &&
             (!pointsMax || pointsMax >= value.pointsMax) &&
             blackList.indexOf(value.uid) === -1 &&
             value.blackList.indexOf(uid) === -1) {
