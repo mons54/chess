01be2a389abfcb220a216c304a84bfc1fd3ee59f
Julien Simonet <j.mons54@gmail.com>
2017-02-05 20:47:16 +0100

    add 1 seconde by turn

--- public/chess.js
+++ public/chess.js
@@ -445,7 +445,7 @@ engine.prototype.init = function (start, end, promotion) {
         this.game.turn50++;
     }
 
-    this.game[this.game.turn].time += this.game.increment;
+    this.game[this.game.turn].time += this.game.increment + 1;
 
     if (this.game[this.game.turn].time > this.game[this.game.turn].timeTurn) {
         this.game[this.game.turn].timeTurn = this.game.timeTurn;
